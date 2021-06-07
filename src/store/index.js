3212d8b27036e368bba30accd75c8f8a14d82020
Hal Helms <hal.helms@gmail.com>
2021-06-07 15:31:03 -0700

    Added leads-related items

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,107 @@ import router from '../router/index'
 export default createStore({
   state: {
     includeArchivedLeads: false,
+    leads: [
+      {
+        status: 'active',
+        date: '12 Jun 2021',
+        contact: 'Ellen Wentz',
+        phone: '702.558.1578',
+        location: 'Henderson, NV',
+        qualification: {
+          desired: 6,
+          desiredRequested: 6,
+          desiredResponses: 4,
+          required: 4,
+          requiredRequested: 4,
+          requiredResponses: 4
+        },
+        transcript: [
+          {
+            origin: 'agent',
+            message: 'Intro/Name Request'
+          },
+          {
+            origin: 'contact',
+            message: 'Ellen Wentz',
+          },
+          {
+            origin: 'agent',
+            message: 'How can I help you today?'
+          },
+          {
+            origin: 'contact',
+            message: 'My water heater is leaking.'
+          },
+          {
+            origin: 'agent',
+            message: "Oh no! Well, let's get you some help with that."
+          },
+          {
+            origin: 'agent',
+            message: 'Location Request'
+          },
+          {
+            origin: 'contact',
+            message: '207 W. Broad, Henderson, NV 89102'
+          },
+          {
+            origin: 'contact',
+            message: 'How soon can someone get out here?'
+          },
+          {
+            origin: 'agent',
+            message: 'Urgency Response'
+          },
+          {
+            origin: 'agent',
+            message: 'Phone Request'
+          },
+          {
+            origin: 'contact',
+            message: '702.558.1578'
+          },
+          {
+            origin: 'agent',
+            message: "Someone should be calling you shortly. Is there anything else I can do for you?"
+          },
+          {
+            origin: 'system',
+            message: 'Chat Terminated'
+          }
+        ]
+      },
+      {
+        status: 'active',
+        date: '12 Jun 2021',
+        contact: 'Davis Thomas',
+        phone: '702.217.5936',
+        location: 'Summerlin, NV',
+        qualification: {
+          desired: 6,
+          desiredRequested: 6,
+          desiredResponses: 3,
+          required: 4,
+          requiredRequested: 4,
+          requiredResponses: 2
+        },
+      },
+      {
+        status: 'archived',
+        date: '18 Jun 2021',
+        contact: 'Andrea Barnes',
+        phone: '702.593.4177',
+        location: 'Las Vegas, NV',
+        qualification: {
+          desired: 6,
+          desiredRequested: 6,
+          desiredResponses: 5,
+          required: 4,
+          requiredRequested: 4,
+          requiredResponses: 3
+        },
+      },
+    ],
   },
   mutations: {
     TOGGLE_INCLUDE_ARCHIVED_LEADS(state) {
@@ -41,6 +142,18 @@ export default createStore({
       // TODO: API call here but mocked for now
         return 4651;
     },
+
+    getLeads: state => includeArchived => {
+      if (includeArchived) {
+        console.log(state.leads)
+        return state.leads
+      } else {
+        return state.leads.filter( lead => {
+          console.log(state.leads)
+          return lead.status == 'active'
+        })
+      }
+    }
 
   },
   actions: {
